b7faf3df81ad1c268664292ea0e61cde9ef9b01b
Gleb-Shalygin <73645931+Gleb-Shalygin@users.noreply.github.com>
2023-02-11 17:11:18 +0600

    First commit

---
+++ resources/js/TestComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>Hello component</h1>
+    <el-input placeholder="Please input" v-model="input"></el-input>
+</template>
+
+<script>
+export default {
+    name: "TestComponent",
+    data() {
+        return {
+            input: ''
+        }
+    }
+}
+</script>
